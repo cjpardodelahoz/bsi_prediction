c69974a4e2a849949b9c4121724e7c42e53c08c6
cjpardodelahoz <cjpardodelahoz@gmail.com>
2025-11-03 13:15:11 -0500

    classfied isabl1 mags with gtdb, ran checkM2, and selected NC Efaecium mags

---
+++ code/scripts/enterococcus_diversity_II/isabl1_gtdb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --output=log/enterococcus_diversity_II/isabl1_gtdb.out
+#SBATCH --error=log/enterococcus_diversity_II/isabl1_gtdb.err
+#SBATCH --time=168:00:00  # Adjust the time as needed
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=256G
+#SBATCH --partition=cpu
+
+# Activate the GTDB environment
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate gtdb
+
+# Set paths
+BIN_DIR="analyses/enterococcus_diversity_II/binning/short/isabl1/vamb/multi_bins"
+OUTPUT_DIR="analyses/enterococcus_diversity_II/binning/short/isabl1/gtdb"
+MASH_DB="/data1/xavierj/carlos/dbs/gtdbtk-2.4.1/db/gtdb_ref_sketch.msh"
+
+# Run the download-db.sh script
+gtdbtk classify_wf --genome_dir ${BIN_DIR} \
+    --out_dir ${OUTPUT_DIR} \
+    --cpus 64 \
+    --mash_db ${MASH_DB}

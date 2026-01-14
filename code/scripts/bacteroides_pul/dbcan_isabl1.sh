6f52de7ea95303cc1bb14d045dd7786a9461f24f
Pardo De La Hoz <pardodc@mski3621.mskcc.root.mskcc.org>
2026-01-14 14:41:07 -0500

    prototype pipeline for PUL and CGC quantification on metagenomes

--- code/scripts/bacteroides_pul/dbcan_isabl1.sh
+++ code/scripts/bacteroides_pul/dbcan_isabl1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --output=log/bacteroides_pul/dbcan_%A_%a.out
 #SBATCH --error=log/bacteroides_pul/dbcan_%A_%a.err
-#SBATCH --array=1-2 #1-2077
+#SBATCH --array=46-47 #1-2076
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
 #SBATCH -p cpu
@@ -11,10 +11,9 @@
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate dbcan
 
-# Sample list
-SAMPLE_FILE="misc_files/enterococcus_diversity/all_target_samples_hostdepleted.txt"
-sample_bracketed=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$SAMPLE_FILE")
-SAMPLE=$(echo $sample_bracketed | sed "s/\[\('\([^']*\)'\)\]/\2/")
+# Get sample name for current task
+SAMPLE_FILE="misc_files/bacteroides_pul/isabl1_samples.txt"
+SAMPLE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$SAMPLE_FILE")
 
 # Directory containing metagenome assemblies
 ASSEMBLY_DIR=analyses/bacteroides_pul/metagenomes/assembly/metaspades
@@ -30,19 +29,14 @@ mkdir -p "$OUTDIR"
 SAMPLE_OUTDIR="$OUTDIR/$SAMPLE"
 mkdir -p "$SAMPLE_OUTDIR"
 
-# Prokka files
-PROKKA_DIR="analyses/bacteroides_pul/pul_prediction/prokka/$SAMPLE"
-PROTEINS="$PROKKA_DIR/${SAMPLE}.faa"
-GFF_FILE="$PROKKA_DIR/${SAMPLE}.gff"
-
 # DBCAN database directory
 DB_DIR="/data1/xavierj/carlos/dbs/dbcan"
 
 # Run dbCAN using easy_substrate workflow
 run_dbcan easy_substrate \
-  --mode protein \
-  --input_raw_data "$PROTEINS" \
-  --input_gff "$GFF_FILE" \
+  --mode prok \
+  --input_raw_data "$CONTIGS" \
+  --input_gff "${SAMPLE_OUTDIR}/uniInput.gff" \
   --gff_type prodigal \
   --output_dir "$SAMPLE_OUTDIR" \
   --db_dir "$DB_DIR" \
---
+++ code/scripts/bacteroides_pul/get_pulabund.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#SBATCH --output=log/bacteroides_pul/get_pulabund_%A_%a.out
+#SBATCH --error=log/bacteroides_pul/get_pulabund_%A_%a.err
+#SBATCH --array=46-47 #1-2076
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64G
+#SBATCH -p cpu
+#SBATCH --time=24:00:00
+
+# Activate conda
+source $(conda info --base)/etc/profile.d/conda.sh
+
+# Set sample and read paths
+# Get sample name (with brackets and quotes) for current task
+sample_bracketed=$(sed -n "${SLURM_ARRAY_TASK_ID}p" misc_files/enterococcus_diversity/all_target_samples_hostdepleted.txt)
+# Remove brackets and single quotes: e.g., ['275C'] -> 275C
+SAMPLE=$(echo $sample_bracketed | sed "s/\[\('\([^']*\)'\)\]/\2/")
+# Get first record for this sample from the paths table
+record=$(awk -v s="$sample_bracketed" '$2==s{print; exit}' misc_files/enterococcus_diversity/all_target_samples_paths.txt)
+# Extract R1 and R2 paths (columns 3 and 4)
+R1=$(echo "$record" | cut -f3)
+R2=$(echo "$record" | cut -f4)
+# Replace storage path
+R1=$(echo "$R1" | sed 's|/data/brinkvd/|/data1/collab004/|')
+R2=$(echo "$R2" | sed 's|/data/brinkvd/|/data1/collab004/|')
+
+# Set directories
+ASSEMBLY_DIR=analyses/bacteroides_pul/metagenomes/assembly/metaspades
+SAM_DIR="analyses/bacteroides_pul/pul_prediction/samfiles"
+ABUND_DIR="analyses/bacteroides_pul/pul_prediction/abund"
+SAMPLE_ABUND_DIR="$ABUND_DIR/$SAMPLE"
+DBCAN_OUT="analyses/bacteroides_pul/pul_prediction/dbcan/${SAMPLE}"
+mkdir -p "$SAM_DIR" "$ABUND_DIR" "$SAMPLE_ABUND_DIR"
+
+# Contigs and FFN files
+CONTIGS="$ASSEMBLY_DIR/$SAMPLE/contigs_1000.fasta"
+#FFN="$DBCAN_OUT/$SAMPLE.ffn"
+
+# 1. Index contigs
+conda activate bwa
+bwa index "$CONTIGS"
+
+# 2. Map reads to CDS and contigs
+bwa mem -t 16 -o "$SAM_DIR/${SAMPLE}.sam" "$CONTIGS" "${R1}" "${R2}"
+
+# 3. Sort SAM files and convert to indexed BAM
+conda activate bowtie2
+samtools sort -@ 16 -o "$SAM_DIR/${SAMPLE}.bam" "$SAM_DIR/${SAMPLE}.sam"
+samtools index "$SAM_DIR/${SAMPLE}.bam"
+
+# 4. Calculate read coverage for all proteins using dbcan_utils
+conda activate dbcan
+dbcan_utils cal_coverage \
+    -g "$DBCAN_OUT/uniInput.gff" \
+    -i "$SAM_DIR/${SAMPLE}.bam" \
+    -o "$SAMPLE_ABUND_DIR/${SAMPLE}.depth.txt" \
+    -t 16
+
+# 5. Estimate abundance of CAZymes, CGCs, and PULs using dbcan_utils
+cd "$SAMPLE_ABUND_DIR"
+dbcan_utils fam_abund \
+    -bt "${SAMPLE}.depth.txt" \
+    -i "../../../../../$DBCAN_OUT" \
+    -o "${SAMPLE}.fam_abund.txt" \
+    -a TPM
+dbcan_utils fam_substrate_abund \
+    -bt "${SAMPLE}.depth.txt" \
+    -i "../../../../../$DBCAN_OUT" \
+    -o "$SAMPLE_ABUND_DIR/${SAMPLE}.fam_substrate_abund.txt" \
+    -a TPM
+dbcan_utils CGC_abund \
+    -bt "${SAMPLE}.depth.txt" \
+    -i "../../../../../$DBCAN_OUT" \
+    --output "$SAMPLE_ABUND_DIR/" \
+    -a TPM
+dbcan_utils CGC_substrate_abund \
+    -bt "${SAMPLE}.depth.txt" \
+    -i "../../../../../$DBCAN_OUT" \
+    -o "$SAMPLE_ABUND_DIR/${SAMPLE}.CGC_substrate_abund.txt" \
+    -a TPM

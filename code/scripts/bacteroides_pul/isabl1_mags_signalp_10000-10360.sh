20634c34997ec78c4dba4784a10104ad1dd68a9b
Pardo De La Hoz <pardodc@mski3621.mskcc.root.mskcc.org>
2026-01-22 18:28:10 -0500

    predicted signal peptides on isabl1 MAGs

--- code/scripts/bacteroides_pul/isabl1_mags_signalp_10000-10360.sh
+++ code/scripts/bacteroides_pul/isabl1_mags_signalp_10000-10360.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --array=1-9999%500
+#SBATCH --array=1-361
 #SBATCH --mem=16G         # Memory per task
 #SBATCH -c 4              # Number of CPU cores per task
 #SBATCH --time=48:00:00   # Maximum runtime
@@ -13,15 +13,12 @@ source $(conda info --base)/etc/profile.d/conda.sh
 conda activate signalp
 
 # Get the MAG name for the current task
-MAG=$(cat analyses/bacteroides_pul/isabl1_nc_mags.txt | sed -n ${SLURM_ARRAY_TASK_ID}p)
+MAG=$(tail -n 361 analyses/bacteroides_pul/isabl1_nc_mags.txt | sed -n ${SLURM_ARRAY_TASK_ID}p)
 
 # Set paths
 PUL_DIR="analyses/bacteroides_pul/mag_pul_prediction/isabl1/${MAG}"
 PROTEINS="${PUL_DIR}/CGC.faa"
 OUT_DIR="${PUL_DIR}/signalp"
-
-# Remove output directory if it exists
-rm -r ${OUT_DIR}
 
 # Run SignalP
 signalp6 --fastafile ${PROTEINS} \
